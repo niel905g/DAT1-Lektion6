0085c9ec49c2d5592feb0cff7fc18cc5b7bf859c
Mathias Bruun Hemmingsen <31951525+tejsfrank@users.noreply.github.com>
2017-10-05 10:27:11 +0200

    Min package :)

--- src/Mathias/Test.java
+++ src/Mathias/Test.java
@@ -1,4 +1,39 @@
 package Mathias;
 
+import java.util.Scanner;
+
 public class Test {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println(" Indtast et tal");
+        int tal = input.nextInt();
+
+
+        if (tal % 5 == 0 && tal % 2 == 0)   {
+            System.out.println(" HighTWO");
+
+        }
+        else if (tal % 5 == 0) {
+            System.out.println(" HiFive");
+        }
+
+        else if (tal % 2 == 0) {
+            System.out.println(" HiEven ");
+        }
+          else System.out.println(" Du har indtastet noget som ikke gir mening ...");
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 }
