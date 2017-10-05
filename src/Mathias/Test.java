b4e8320663f21f86a3d8fedba58bfb28876815e5
Mathias Bruun Hemmingsen <31951525+tejsfrank@users.noreply.github.com>
2017-10-05 10:47:36 +0200

    Min package :)

--- src/Mathias/Test.java
+++ src/Mathias/Test.java
@@ -6,18 +6,18 @@ public class Test {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println(" Indtast et tal");
-        int tal = input.nextInt();
+        System.out.println(" Indtast et tal"); // printer en linje i konsollen som skriver indtast et tal
+        int tal = input.nextInt(); // Deklarerer variablen tal som et input i konsollen
 
-
+         // Hvis tallet kan ganges med 5 eller divideres med 2 så skriver den HIGHTWO i konsollen.
         if (tal % 5 == 0 && tal % 2 == 0)   {
             System.out.println(" HighTWO");
 
-        }
+        }    // Hvis tallet der indtastes kan ganges med 5 skriver den HiFive
         else if (tal % 5 == 0) {
             System.out.println(" HiFive");
         }
-
+              // hvis tallet kan divideres med 2 skriver den HiEven
         else if (tal % 2 == 0) {
             System.out.println(" HiEven ");
         }
